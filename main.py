999a566ed7e35b944ffa5ea87a47ca53cf75486e
Salominho <salomaomoraes1@hotmail.com>
2024-11-21 04:36:30 -0300

    main

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ import os
 # Adicionar diretorio Pai para verificação no caminho de busca de módulos no Python
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from Clinica import Paciente
-from Eventos import Evento
-from Restaurante import Restaurante
+from Clinica import HistoricoPacientes, GerenciadorEmergencia, CadastroMedicos
+from Eventos import HistoricoEventos, FilaDeEventos, ArvoreParticipantes
+from Restaurante import Pedido, 
 
 class Main:
     def __init__(self):
         # Clinica
-        self.paciente = Paciente()
+        self.paciente = HistoricoPacientes()
+        self.emergencia = GerenciadorEmergencia()
+        self.medico = CadastroMedicos()
         # Eventos
-        self.eventos = Evento()
+        self.historico = HistoricoEventos()
+        self.eventos = FilaDeEventos()
+        self.participantes = ArvoreParticipantes()
         # Restaurante
-        self.restaurante = Restaurante()
+        self.pedido = Pedido()
         
     def run(self):
         self.janela_principal.mainloop()
